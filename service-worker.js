30036744b49f762eedd66b1d20cfd9be6c8deb9a
Preston Chen <chen.preston@gmail.com>
2025-05-01 18:41:24 +1200

    v0.7.5
    update school term weekdays database

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "shift-helper-cache-v0.7.5"; // 更新缓存时修改版本号
+const CACHE_NAME = "shift-helper-cache-v0.7.6"; // 更新缓存时修改版本号
 const CACHE_PREFIX = CACHE_NAME.split("-v")[0]; // 提取前缀 
 const urlsToCache = [
   "/",
